1e5fa312e31ef0f0aeb0fd047fecf7ed3a397d61
Gestor Financeiro Deploy <ran@example.com>
2026-08-20 13:55:21 +0100

    Deploy v1.75.0+103 (2026-08-20 13:55)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3626219142" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "623843552" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
