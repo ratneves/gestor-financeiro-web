ae99d86e094f945172c40da9607d403f9ba3c1ac
ratneves <raneves@gmail.com>
2026-08-31 14:31:24 +0100

    v1.99.0+127: fix base-href for GitHub Pages subpath

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2802063784" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2106537533" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
